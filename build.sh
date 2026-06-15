f2474b1bf5c0b5d696c5728923bd9a61ec817154
D Ξ Ξ Z N U T Z <daizeuzdred@gmail.com>
2026-06-15 18:56:04 +0800

    Update build script to use new zip filename format (wrong branch lol)

--- build.sh
+++ build.sh
@@ -69,8 +69,8 @@ rm -rf AnyKernel
 git clone --depth=1 https://github.com/daizeuz-dred/AnyKernel3 -b apex AnyKernel
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
-zip -r9 APEX-Kernel-v1.0-OC-${TANGGAL}.zip *
-curl -F "file=@APEX-Kernel-v1.0-OC-${TANGGAL}.zip" https://store1.gofile.io/uploadFile
+zip -r9 APEX-Kernel-v1.0-${TANGGAL}.zip *
+curl -F "file=@APEX-Kernel-v1.0-${TANGGAL}.zip" https://store1.gofile.io/uploadFile
 cd ../
 fi
 }
